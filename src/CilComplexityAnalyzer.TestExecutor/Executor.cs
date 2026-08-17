af273be26f5b334da618d93bdc11a3e030e8f91e
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-08-17 17:04:07 +0200

    Update docker image tag

--- src/CilComplexityAnalyzer.TestExecutor/Executor.cs
+++ src/CilComplexityAnalyzer.TestExecutor/Executor.cs
@@ -11,7 +11,8 @@ namespace CilComplexityAnalyzer.TestExecutor;
 
 internal static class Executor
 {
-    private static readonly IImage DockerImage = new DockerImage("TODO");
+    private const string DockerImageTag = "docker.io/kamil7430/cil-complexity-analyzer-container-worker:latest";
+    private static readonly IImage DockerImage = new DockerImage(DockerImageTag);
     
     internal static void Initialize(ILogger? logger)
     {
