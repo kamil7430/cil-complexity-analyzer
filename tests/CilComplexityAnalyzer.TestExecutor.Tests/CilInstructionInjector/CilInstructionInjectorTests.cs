12d2997b2b1b299ee424d9a6dda76b729cfe9056
Major FalleN <kgrobel3405@o2.pl>
2026-09-09 00:22:18 +0200

    Refactor AssertionClass and added branch target test

--- tests/CilComplexityAnalyzer.TestExecutor.Tests/CilInstructionInjector/CilInstructionInjectorTests.cs
+++ tests/CilComplexityAnalyzer.TestExecutor.Tests/CilInstructionInjector/CilInstructionInjectorTests.cs
@@ -59,9 +59,46 @@ public class CilInstructionInjectorTests
         testSuite.InjectCilToStudentSolution();
 
         // Assert
-        testSuite.ShouldHaveInstructionSeqeunceInjected(
+        testSuite.ShouldHaveInstructionSequenceInjected(
             className: "Calculator", 
             methodName: "Add"
+        );
+    }
+    
+    [TestMethod]
+    public void InjectCilToStudentSolution_RedirectsBranchTargetsToInjectedCounters()
+    {
+        // Arrange
+        const string studentCode = @"
+        namespace StudentSolution;
+        public class Calculator
+        {
+            public int Max(int a, int b)
+            {
+                if (a > b)
+                {
+                    return a;
+                }
+                return b;
+            }
+        }";
+
+        var originalTestSuite = CilTestSuiteBuilder.Create()
+            .WithStudentCode(studentCode)
+            .Build();
+
+        var testSuite = CilTestSuiteBuilder.Create()
+            .WithStudentCode(studentCode)
+            .Build();
+
+        // Act
+        testSuite.InjectCilToStudentSolution();
+
+        // Assert
+        testSuite.ShouldRedirectBranchTargetsToCorrectInjectedCounter(
+            originalTestSuite: originalTestSuite,
+            className: "Calculator",
+            methodName: "Max"
         );
     }
 }
