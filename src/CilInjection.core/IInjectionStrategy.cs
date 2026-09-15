06f4213048541145153102c77f50b354b33ade2d
Major FalleN <kgrobel3405@o2.pl>
2026-09-15 21:25:09 +0200

    Restructurize whole CilInjectiong architecture

---
+++ src/CilInjection.core/IInjectionStrategy.cs
@@ -0,0 +1,16 @@
+﻿namespace CilInjection.Core;
+
+using Mono.Cecil;
+using System.Runtime.Loader;
+
+public interface IInjectionStrategy
+{
+    // CIL Weaving: modyfikuje bajty Cecil
+    void Inject(ModuleDefinition module);
+
+    // Metoda dostarczająca ścieżkę do właściwego RunTime.dll
+    string GetRuntimeAssemblyPath();
+
+    // Fabryka uchwytu dla użytkownika po załadowaniu ALC
+    object CreateHandle(AssemblyLoadContext context);
+}
---
+++ src/CilInjection.core/InjectionPipeline.cs
@@ -0,0 +1,54 @@
+﻿namespace CilInjection.Core;
+
+using Mono.Cecil;
+
+public class InjectionPipeline
+{
+    // Lista zarejestrowanych strategii (pole klasy)
+    private readonly List<IInjectionStrategy> _strategies = new();
+
+    /// <summary>
+    /// Rejestruje nową strategię iniekcji w potoku.
+    /// </summary>
+    public InjectionPipeline AddStrategy(IInjectionStrategy strategy)
+    {
+        ArgumentNullException.ThrowIfNull(strategy);
+        _strategies.Add(strategy);
+        return this;
+    }
+
+    /// <summary>
+    /// Przetwarza strumień bajtów pliku .dll przez wszystkie zarejestrowane strategie.
+    /// </summary>
+    public byte[] Transform(byte[] assemblyBytes)
+    {
+        if (_strategies.Count == 0)
+            return assemblyBytes; // Brak strategii = brak zmian
+
+        using var stream = new MemoryStream(assemblyBytes);
+        using var module = ModuleDefinition.ReadModule(stream);
+
+        // Aplikujemy każdą strategię po kolei na TYM SAMYM module (jednokrotny odczyt)
+        foreach (var strategy in _strategies)
+        {
+            strategy.Inject(module);
+        }
+
+        using var outputStream = new MemoryStream();
+        module.Write(outputStream);
+        return outputStream.ToArray();
+    }
+
+    /// <summary>
+    /// Zwraca listę ścieżek do plików RunTime.dll wymaganych przez zarejestrowane strategie.
+    /// </summary>
+    public IEnumerable<string> GetRequiredRuntimePaths()
+    {
+        return _strategies.Select(s => s.GetRuntimeAssemblyPath()).Distinct();
+    }
+
+    /// <summary>
+    /// Pobiera zarejestrowane strategie (np. do późniejszego wygenerowania uchwytów w ALC).
+    /// </summary>
+    public IReadOnlyList<IInjectionStrategy> Strategies => _strategies.AsReadOnly();
+}
