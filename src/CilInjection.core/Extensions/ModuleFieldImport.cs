6a919192ae6954bc573dbae2bc749af27a7cc9f4
Major FalleN <kgrobel3405@o2.pl>
2026-09-15 23:55:06 +0200

    Quick Injecting Refactor

---
+++ src/CilInjection.core/Extensions/ModuleFieldImport.cs
@@ -0,0 +1,45 @@
+﻿namespace CilInjection.Core.Extensions;
+
+using System;
+using System.Reflection;
+using Mono.Cecil;
+
+public static class ModuleFieldImportExtensions
+{
+    /// <summary>
+    /// Importuje referencję do statycznego pola z dowolnej klasy RunTime.
+    /// Typ pola jest automatycznie rozpoznawany na podstawie refleksji.
+    /// </summary>
+    public static FieldReference ImportStaticField(
+        this ModuleDefinition targetModule,
+        Type containerType,
+        string fieldName)
+    {
+        ArgumentNullException.ThrowIfNull(targetModule);
+        ArgumentNullException.ThrowIfNull(containerType);
+        
+        var fieldInfo = containerType.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
+                        ?? throw new MissingFieldException(containerType.FullName, fieldName);
+
+        var assemblyName = containerType.Assembly.GetName();
+
+        var assemblyRef = new AssemblyNameReference(
+            assemblyName.Name!,
+            assemblyName.Version);
+
+        var typeRef = new TypeReference(
+            @namespace: containerType.Namespace!,
+            name: containerType.Name,
+            module: targetModule,
+            scope: assemblyRef);
+
+        var fieldTypeRef = targetModule.ImportReference(fieldInfo.FieldType);
+
+        var fieldRef = new FieldReference(
+            name: fieldName,
+            fieldType: fieldTypeRef,
+            declaringType: typeRef);
+
+        return targetModule.ImportReference(fieldRef);
+    }
+}
--- src/CilInstructionCounter.Provider/CilInjector.cs
+++ src/CilInstructionCounter.Provider/CilInjector.cs
@@ -1,9 +1,6 @@
 ﻿namespace CilInstructionCounter;
 
-using System.Runtime.Loader;
-using CilInjection.Core;
 using CilInjection.Core.Extensions;
-using CilInjection.Core.Weaver;
 using RunTime;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -12,49 +9,18 @@ internal static class CilInjector
 {
     public static void Inject(this ModuleDefinition module)
     {
-        // Rejestrujemy referencję do zewnętrznego pola w InstructionCounter.RunTime.dll
-        var counterFieldRef = ImportRuntimeCounterField(module);
+        // 1. Bezpieczny import statycznego pola (sam wykrywa, że to long / Int64)
+        var counterFieldRef = module.ImportStaticField(
+            typeof(GlobalCounterContainer), 
+            nameof(GlobalCounterContainer.InstructionCounter));
 
-        // Przechodzimy po typach i metodach
-        foreach (var type in module.GetAllTypesRecursively())
+        // 2. Iniekcja CIL we wszystkich metodach modułu
+        module.InjectInEveryMethodAtInstructionLevel((il, targetInstr) => new[]
         {
-            if (type.IsInterface) continue;
-
-            foreach (var method in type.Methods)
-            {
-                if (!method.HasBody) continue;
-
-                // Wywołujemy bezpieczny silnik CIL przekazując tylko opkody podbijające licznik
-                method.InjectAtInstructionLevel((il, targetInstr) => new[]
-                {
-                    il.Create(OpCodes.Ldsfld, counterFieldRef),
-                    il.Create(OpCodes.Ldc_I8, 1L),
-                    il.Create(OpCodes.Add),
-                    il.Create(OpCodes.Stsfld, counterFieldRef)
-                });
-            }
-        }
-    }
-    
-    private static FieldReference ImportRuntimeCounterField(ModuleDefinition targetModule)
-    {
-        var runtimeAssemblyName = typeof(GlobalCounterContainer).Assembly.GetName();
-
-        var assemblyRef = new AssemblyNameReference(
-            runtimeAssemblyName.Name!,
-            runtimeAssemblyName.Version);
-
-        var typeRef = new TypeReference(
-            @namespace: typeof(GlobalCounterContainer).Namespace!,
-            name: nameof(GlobalCounterContainer),
-            module: targetModule,
-            scope: assemblyRef);
-
-        var fieldRef = new FieldReference(
-            name: nameof(GlobalCounterContainer.InstructionCounter),
-            fieldType: targetModule.TypeSystem.Int64,
-            declaringType: typeRef);
-
-        return targetModule.ImportReference(fieldRef);
+            il.Create(OpCodes.Ldsfld, counterFieldRef),
+            il.Create(OpCodes.Ldc_I8, 1L),
+            il.Create(OpCodes.Add),
+            il.Create(OpCodes.Stsfld, counterFieldRef)
+        });
     }
 }
